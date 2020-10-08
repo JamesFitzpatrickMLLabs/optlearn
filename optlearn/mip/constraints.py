104f4892bf66d1a6678b794ed54d8c5a6c4a1748
James Fitzpatrick <james.fitzpatrick1@ucdconnect.ie>
2020-10-08 11:57:22 +0100

    added constraints.py

---
+++ optlearn/mip/constraints.py
@@ -0,0 +1,34 @@
+import networkx as nx
+
+from optlearn.mip import mip_utils
+
+
+def initialise_mincut_graph(is_symmetric=True):
+    """ Set the mincut graph """
+        
+    if is_symmetric:
+        graph_type = nx.Graph
+    else:
+        graph_type = nx.DiGraph
+    return graph_type()
+
+
+def set_mincut_graph(graph, edges, values):
+    """ Build a graph structure for mincut """
+
+    edge_bunch = [(*edge, {"capacity": value}) for edge, value in zip(edges, values)]
+    graph.add_edges_from(edge_bunch)
+    return graph
+
+
+def get_mincut_variables(graph, vertex_a, vertex_b, variable_dict, is_symmetric=True):
+    """ Get the variables needed to build a mincut constraint for a specific vertex pair """
+
+    cut_value, (set_a, set_b) = nx.minimum_cut(graph, vertex_a, vertex_b)
+    
+    if cut_value <= 0.99 + is_symmetric * 1:
+        keys = variable_dict.keys()
+        keys = [key for key in keys if (mip_utils.get_variable_tuple(key)[0] in set_a and
+                                        mip_utils.get_variable_tuple(key)[1] in set_a)]
+        return [variable_dict[key] for key in keys]
+    return None
