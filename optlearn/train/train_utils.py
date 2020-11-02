c69d77779fed54c5c52f2f7bb55539e1719f288d
James Fitzpatrick <james.fitzpatrick1@ucdconnect.ie>
2020-11-02 18:08:34 +0000

    moved train_utils.py

---
+++ optlearn/train/train_utils.py
@@ -0,0 +1,22 @@
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+
+
+def accuracy(y, y_hat):
+    """ Compute the accuracy """
+
+    return accuracy_score(y, y_hat)
+
+
+def true_positive_rate(y, y_hat):
+    """ Compute the true positive rate """
+
+    return recall_score(y, y_hat)
+
+
+def false_negative_rate(y, y_hat):
+    """ Compute the false negative rate """
+
+    return 1 - true_positive_rate(y, y_hat)
+
+
