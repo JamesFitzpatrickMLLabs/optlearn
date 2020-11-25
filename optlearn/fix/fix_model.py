2f7e4d41086d63d55d4a6fafc178ef948dc29be9
James Fitzpatrick <james.fitzpatrick1@ucdconnect.ie>
2020-11-25 10:51:46 +0000

    bugfixed /fix/fix_model.py

--- optlearn/fix/fix_model.py
+++ optlearn/fix/fix_model.py
@@ -43,7 +43,7 @@ def ensure_doubletree(original_graph, pruned_graph):
 
     
     model = mst_model.doubleTreeSparsifier()
-    iterations = int(np.ceil(np.log2(len(graph.nodes))))
+    iterations = int(np.ceil(np.log2(len(original_graph.nodes))))
     new_edge_sets = run_sparsify(original_graph, iterations)
 
     for new_edges in new_edge_sets:
