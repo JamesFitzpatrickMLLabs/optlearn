4690ea2a4dc0c8b4fcb9b65680df8c67f2d4e8e8
James Fitzpatrick <james.fitzpatrick1@ucdconnect.ie>
2020-11-25 16:01:19 +0000

    updated /fix/fix_model.py

--- optlearn/fix/fix_model.py
+++ optlearn/fix/fix_model.py
@@ -38,12 +38,14 @@ def ensure_connected(original_graph, pruned_graph, threshold=None):
     return fix_utils.migrate_edges(original_graph, pruned_graph, new_edges)
 
 
-def ensure_doubletree(original_graph, pruned_graph):
+def ensure_doubletree(original_graph, pruned_graph, iterations=None):
     """ Make sure that the graph at least has two doubletree edge sets """
 
-    
+
+    if iterations is None:
+        iterations = int(np.ceil(np.log2(len(original_graph.nodes))))
+        
     model = mst_model.doubleTreeSparsifier()
-    iterations = int(np.ceil(np.log2(len(original_graph.nodes))))
     new_edge_sets = model.run_sparsify(original_graph, iterations)
 
     for new_edges in new_edge_sets:
