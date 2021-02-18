42d0bd889bbb0eb59064f661a6f781075e0d5e20
James Fitzpatrick <james.fitzpatrick1@ucdconnect.ie>
2021-02-18 12:16:09 +0000

    updated /feature/features.py

--- optlearn/feature/features.py
+++ optlearn/feature/features.py
@@ -1,11 +1,13 @@
+import random
+
+import numpy as np
+
 from optlearn import graph_utils
 
 from optlearn.quad import quad_features
 from optlearn.mst import mst_features
 from optlearn.mst import mst_model
 from optlearn.mip import mip_model
-
-import numpy as np
 
 
 def compute_f1_edge(graph, vertex_a, vertex_b, self_max=False):
@@ -183,26 +185,28 @@ def compute_f4_vertices(graph, self_max=False):
                      for vertex in vertices]).T.flatten()
 
 
-def compute_f5_edges(graph, tours, edges_in, sort=False):
+def compute_f5_edges(graph, sort=False):
     """ Compute feature f5 of Sun et al. """
     """ Expects a set of tours and an array indicating if each edge 
         is in each tour """
 
-    if not sort:
-        tours = graph_utils.sort_tours_by_length(graph, tours - 1) + 1
+    tours = np.array([random.sample(graph.nodes, len(graph.nodes)) for i in range(10000)])
+    edges_in = graph_utils.hash_edges_in_tours(graph.edges, tours)
+    tours = graph_utils.sort_tours_by_length(graph, tours)
     tour_ranks = np.expand_dims(np.arange(len(tours)) + 1, 1)
     scores = np.sum(edges_in / tour_ranks, axis=0)
     return scores / np.max(scores)
     
 
-def compute_f6_edges(graph, tours, edges_in, sort=False):
+def compute_f6_edges(graph):
     """ Compute feature f6 of Sun et al. """
     """ Expects a set of tours and an array indicating if each edge 
         is in each tour """
 
-    if not sort:
-        tours = graph_utils.sort_tours_by_length(graph, tours - 1) + 1
-    tour_lengths = graph_utils.compute_tour_lengths(graph, tours - 1)
+    tours = np.array([random.sample(graph.nodes, len(graph.nodes)) for i in range(10000)])
+    edges_in = graph_utils.hash_edges_in_tours(graph.edges, tours)
+    tours = graph_utils.sort_tours_by_length(graph, tours)
+    tour_lengths = graph_utils.compute_tour_lengths(graph, tours)
     numerator_a = (edges_in - edges_in.mean(axis=0))
     numerator_b = np.expand_dims(tour_lengths - tour_lengths.mean(), 1)
     numerator = np.sum(numerator_a * numerator_b, axis=0)
@@ -210,7 +214,7 @@ def compute_f6_edges(graph, tours, edges_in, sort=False):
     denominator_b = np.power(np.sum(np.power(numerator_b,2)), 0.5)
     denominator = denominator_a * denominator_b
     scores =  np.nan_to_num(numerator / denominator, 0)
-    return scores / np.max(scores)
+    return scores / np.min(scores)
 
 
 def compute_f7_edges(graph, iterations="auto"):
@@ -271,6 +275,125 @@ def compute_f13_edges(graph):
     return np.log(10) / np.log(order)
 
 
+def compute_fa_edges(graph):
+    """ Compute the weight divided by the global max """
+
+    weights = np.array(graph_utils.get_weights(graph))
+    global_max = weights.max()
+    return  (1 + weights) / (1 + global_max)
+
+
+def compute_fb_edges(graph):
+    """ Compute the weight divided by the max left neighbour weight """
+
+    min_vertex = np.min(graph.nodes)
+    weights = 1 + np.array(graph_utils.get_weights(graph)) 
+    maxes = [1 + np.max(graph_utils.get_edge_weights(graph, node)) for node in graph.nodes]
+    return np.array([weight / maxes[edge[0] - min_vertex]
+                     for weight, edge in zip(weights, graph.edges)])
+
+
+def compute_fc_edges(graph):
+    """ Compute the weight divided by the max right neighbour weight """
+
+    min_vertex = np.min(graph.nodes)
+    weights = 1 + np.array(graph_utils.get_weights(graph)) 
+    maxes = [1 + np.max(graph_utils.get_edge_weights(graph, node, out=False)) for
+             node in graph.nodes]
+    return np.array([weight / maxes[edge[1] - min_vertex] for
+                     weight, edge in zip(weights, list(graph.edges))])
+
+
+def compute_fd_edges(graph):
+    """ Compute the weight divided by the global max """
+
+    weights = np.array(graph_utils.get_weights(graph))
+    global_min = weights.min()
+    return  (1 + global_min) / (1 + weights) 
+
+
+def compute_fe_edges(graph):
+    """ Compute the weight divided by the min left neighbour weight """
+
+    min_vertex = np.min(graph.nodes)
+    weights = 1 + np.array(graph_utils.get_weights(graph)) 
+    mins = [1 + np.min(graph_utils.get_edge_weights(graph, node)) for
+            node in graph.nodes]
+    return np.array([mins[edge[0] - min_vertex] / weight for
+                     weight, edge in zip(weights, graph.edges)])
+
+
+def compute_ff_edges(graph):
+    """ Compute the weight divided by the min right neighbour weight """
+
+    min_vertex = np.min(graph.nodes)
+    weights = 1 + np.array(graph_utils.get_weights(graph)) 
+    mins = [1 + np.min(graph_utils.get_edge_weights(graph, node, out=False)) for
+            node in graph.nodes]
+    return np.array([mins[edge[1]-min_vertex] / weight for
+                     weight, edge in zip(weights, graph.edges)])
+
+
+def compute_fg_edges(graph):
+    """ Continuous features from the MWST extraction method  """
+
+    model = mst_model.mstSparsifier()
+    return mst_features.build_prune_features(model, graph)
+
+
+def compute_fh_edges(graph):
+    """ Compute the root relxation features for each edge """
+
+    problem = mip_model.tspProblem(
+        solver="scip",
+        var_type="continuous",
+        graph=graph,
+        verbose=False
+    )
+
+    problem.optimise(max_rounds=7)
+    return problem.get_varvals()
+
+
+def compute_fi_edges(graph):
+    """ Compute the root relxation features for each edge """
+
+    problem = mip_model.tspProblem(
+        solver="scip",
+        var_type="continuous",
+        graph=graph,
+        verbose=False
+    )
+
+    problem.optimise(max_rounds=7)
+    costs = np.array(problem.get_redcosts())
+    return costs / costs.max()
+
+
+def compute_fj_edges(graph):
+    """ Compute the bet-and-run relaxation reduced costs for each edge """
+
+    problem = mip_model.tspProblem(
+        solver="scip",
+        var_type="continuous",
+        graph=graph,
+        verbose=False,
+        shuffle_columns=False,
+        perturb=True,
+    )
+    rounds = int(np.ceil(np.log2(len(graph.edges))))
+    costs = []
+
+    for i in range(rounds):
+        problem.optimise(max_rounds=1)
+        costs.append(problem.get_redcosts())
+        problem.problem.freeTransform()
+        problem.set_objective(graph)
+    costs = np.sum(costs, axis=0)
+    return costs / costs.max()
+
+
+
 
 functions = {
     "compute_f1_edges": compute_f1_edges,
@@ -290,5 +413,15 @@ functions = {
     "compute_f2_vertices": compute_f2_vertices,
     "compute_f3_vertices": compute_f3_vertices,
     "compute_f4_vertices": compute_f4_vertices,
+    "compute_fa_edges": compute_fa_edges,
+    "compute_fb_edges": compute_fb_edges,
+    "compute_fc_edges": compute_fc_edges,
+    "compute_fd_edges": compute_fd_edges,
+    "compute_fe_edges": compute_fe_edges,
+    "compute_ff_edges": compute_ff_edges,
+    "compute_fg_edges": compute_fg_edges,
+    "compute_fh_edges": compute_fh_edges,
+    "compute_fi_edges": compute_fi_edges,
+    "compute_fj_edges": compute_fj_edges,
     }
 
