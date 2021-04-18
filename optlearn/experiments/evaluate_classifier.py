4b5b50de9410a179f7ec5af79ea05bdca39dfe81
James Fitzpatrick <james.fitzpatrick1@ucdconnect.ie>
2021-04-19 00:36:56 +0100

    trimmed /experiments/evaluate_classifier.py

--- optlearn/experiments/evaluate_classifier.py
+++ optlearn/experiments/evaluate_classifier.py
@@ -16,30 +16,7 @@ from optlearn.fix import fix_model
 from optlearn.fix import fix_utils
 from optlearn.io import write_tsplib
 from optlearn.train import model_utils
-
-
-def load_and_wrap_model(model_path):
-    """ Load a trained model and wrap it to allow easy inference """
-
-    persister = model_utils.modelPersister()
-    persister.load(model_path)
-    return model_utils.modelWrapper(model=persister.model,
-                                    threshold=persister.threshold,
-                                    function_names=persister.function_names) 
-
-
-def load_problem(problem_path):
-    """ Given the path to a tsp problem, load it as a networkx complete graph """
-
-    object = io_utils.optObject().read_problem_from_file(problem_path)
-    return object.get_graph()
-
-
-def load_solution(solution_path):
-    """ Load the solution at the given path """
-
-    object = io_utils.read_solution_from_file(solution_path)
-    return object.get_solution()
+from optlearn.experiments import experiment_utils
 
 
 def compute_solution_objective(graph, problem_path, solution_path=None):
@@ -53,7 +30,7 @@ def compute_solution_objective(graph, problem_path, solution_path=None):
     """
 
     if solution_path is not None:
-        solution = load_solution(solution_path)
+        solution = experiment_utils.load_solution(solution_path)
         if np.min(solution) < np.min(graph.nodes):
             solution += np.min(graph.nodes) - np.min(solution)
         if np.min(graph.nodes) < np.min(solution):
@@ -63,21 +40,6 @@ def compute_solution_objective(graph, problem_path, solution_path=None):
         optimal_objective = solve_for_objective(problem_path)
 
     return optimal_objective
-
-
-
-def check_problems(problem_dir):
-    """ Make sure the files in the directory are all in TSPLIB format """
-
-    try:
-        files = [item.split(".")[1] for item in os.listdir(problem_dir)]
-    except Exception as exception:
-        print("Something went wrong, checking if all files in problem direcotry are valid!")
-        raise exception
-    checks = [item == "tsp" for item in files]
-    if np.sum(checks) < len(checks):
-        print("Found invalid files in problem_dir!")
-        raise ValueError
 
 
 def match_problems_to_solutions(problem_dir, solution_dir=None):
@@ -105,31 +67,6 @@ def match_problems_to_solutions(problem_dir, solution_dir=None):
     
     return pairs
 
-
-def prune_graph(graph, wrapper):
-    """ Prune the given graph, using the wrapper provided """
-
-    y_pred = wrapper.predict_graph(graph)
-    pruned_graph = type(graph)()
-    pruned_graph.add_nodes_from(graph.nodes)
-    retain_edges = np.array(list(graph.edges))[np.array(y_pred).astype(bool)]
-    pruned_graph = fix_utils.migrate_edges(graph, pruned_graph, retain_edges)
-    
-    return pruned_graph
-
-
-def ensure_feasible(original_graph, pruned_graph):
-    """ Given a graph and its pruned counterpart, ensure the pruned one is feasible """
-
-    return fix_model.ensure_doubletree(original_graph, pruned_graph, iterations=1)
-
-
-def prune_graph_feasibly(graph, wrapper):
-    """ Prune the given graph, using the wrapper provided, ensuring feasibility """
-    
-    pruned_graph = prune_graph(graph, wrapper)
-    return ensure_feasible(graph, pruned_graph)
-    
 
 def solve_for_objective(problem_path):
     """ Solve the given problem for its objective value """
@@ -161,15 +98,15 @@ def evaluate_tsp_problem(wrapper, problem_path, temp_path, solution_path=None):
     optmality ratio and problem order
     """
 
-    original_graph = load_problem(problem_path)
+    original_graph = experiment_utils.load_problem(problem_path)
     original_objective = compute_solution_objective(original_graph, problem_path, solution_path)
-    pruned_graph = prune_graph_feasibly(original_graph, wrapper)
+    pruned_graph = wrapper.prune_graph_with_logic(original_graph)
     write_pruned_graph_tsplib(temp_path, original_graph, pruned_graph)
     pruned_objective = compute_solution_objective(original_graph, temp_path)
     
     optimality_ratio = pruned_objective / original_objective
     pruning_rate = 1 - len(pruned_graph.edges) / len(original_graph.edges)
-    problem_order = len(original_graph.edges)
+    problem_order = len(original_graph.nodes)
 
     return {"optimality_ratio": optimality_ratio,
             "pruning_rate": pruning_rate,
@@ -187,7 +124,7 @@ def evaluate_tsp_problems(model_path, problem_dir, solution_dir, temp_path, resu
     problem_orders, optimality_ratios = [], []
     
     try:
-        check_problems(problem_dir)
+        experiment_utils.check_problems(problem_dir)
     except Exception as exception:
         print("Something went wrong, checking if all files in problem dir are valid!")
         raise exception
@@ -202,8 +139,8 @@ def evaluate_tsp_problems(model_path, problem_dir, solution_dir, temp_path, resu
         raise exception
     
     try:
-        print("Loading and warpping trained model at {}!".format(model_path))
-        wrapper = load_and_wrap_model(model_path)
+        print("Loading and wrapping trained model at {}!".format(model_path))
+        wrapper = experiment_utils.load_and_wrap_model(model_path)
     except Exception as exception:
         print("Could not load the model and wrap it!")
         raise exception
