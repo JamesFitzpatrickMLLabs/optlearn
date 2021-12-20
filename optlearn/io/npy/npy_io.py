f9b4ce6cc2a0191cf11ec99dff214aa64f5347bf
James Fitzpatrick <james.fitzpatrick1@ucdconnect.ie>
2021-12-20 17:27:08 +0000

    added optlearn/io/npy/npy_io.py

---
+++ optlearn/io/npy/npy_io.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def load_npy_file(path):
+    """ Load a single npy file (in a context manager) """
+
+    with  np.load(path) as npy:
+        return npy
+
+def load_npz_file(path):
+    """ Load a single npz training file """
+    with load_npy_file(path) as npz:
+        return npz["X"], npz["y"]
+
+
+def load_npz_datas_for_training(paths):
+    """ Load many npz training files into X, y arrays """
+    
+    pairs = [load_npz_file(path) for path in paths]
+    X = np.vstack([pair[0] for pair in pairs])
+    y = np.concatenate([pair[1] for pair in pairs])
+    return X, y
