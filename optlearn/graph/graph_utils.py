900483713084f8d642b357a800a70555f7413af4
James Fitzpatrick <james.fitzpatrick1@ucdconnect.ie>
2021-12-20 17:29:04 +0000

    added optlearn/graph/graph_utils.py

---
+++ optlearn/graph/graph_utils.py
@@ -0,0 +1,17 @@
+import networkx as nx
+
+
+def create_graph():
+    """ Create an empty graph """
+
+    graph = nx.Graph()
+
+    return graph
+
+
+def create_digraph():
+    """ Create an empty digraph """
+
+    digraph = nx.DiGraph()
+
+    return digraph
